e000b807544ba79f0e388d92b25cfda69e087828
IamBingBing <03lonnie@naver.com>
2020-03-20 15:22:32 +0900

    시플님 덕분에 고쳤음

--- BingBingSetServer/src/server/serverset.php
+++ BingBingSetServer/src/server/serverset.php
@@ -6,7 +6,6 @@ use pocketmine\utils\Config;
 
 class serverset extends PluginBase implements Listener{
     public function onEnable(){
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->set = new Config($this->getDataFolder()."setting.yml" , Config::YAML , ["maxplayer" => 2020 , "servernameinfo" => "Hash 서버 :: 제국의 시작"]);
         $this->s = $this->set->getAll();
         $this->getServer()->getQueryInformation()->setMaxPlayerCount($this->s["maxplayer"]);
