bc7630c172f12189618cf816b3e7d2a582aba90b
IamBingBing <03lonnie@naver.com>
2020-03-20 15:03:43 +0900

    Add files via upload

---
+++ BingBingSetServer/src/server/serverset.php
@@ -0,0 +1,15 @@
+<?php
+namespace server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+
+class serverset extends PluginBase implements Listener{
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->set = new Config($this->getDataFolder()."setting.yml" , Config::YAML , ["maxplayer" => 2020 , "servernameinfo" => "Hash 서버 :: 제국의 시작"]);
+        $this->s = $this->set->getAll();
+        $this->getServer()->getQueryInformation()->setMaxPlayerCount($this->s["maxplayer"]);
+        $this->getServer()->getQueryInformation()->setServerName($this->s["servernameinfo"]);
+    }
+}
